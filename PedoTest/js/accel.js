efd27e1316484a7709fc2c025e5e9747510fb66b
Techlogic <nils.heyer@gmx.de>
2015-03-26 13:02:59 +0100

    Fixed background errors

--- PedoTest/js/accel.js
+++ PedoTest/js/accel.js
@@ -12,10 +12,18 @@
 		accelX = e.acceleration.x;
 		accelY = -(e.acceleration.y);
 		accelZ = -(e.acceleration.z);
+		rotx = e.rotationRate.alpha ;
+		roty = e.rotationRate.beta ;
+		rotz = e.rotationRate.gamma ;
     	console.log(accelX);
     	
-		document.getElementById("accelX").innerHTML =  'Accel X : ' + accelX;
-	   	document.getElementById("accelY").innerHTML = 'Accel Y : ' + accelY;
-	   	document.getElementById("accelZ").innerHTML = 'Accel Z : ' + accelZ;
+		document.getElementById("accelX").innerHTML =  'Accel X : ' + Math.round(accelX*100)/100;
+	   	document.getElementById("accelY").innerHTML = 'Accel Y : ' + Math.round(accelY*100)/100;
+	   	document.getElementById("accelZ").innerHTML = 'Accel Z : ' + Math.round(accelZ*100)/100;
+	   	document.getElementById("rotX").innerHTML = "Rot X :" + Math.round(rotx*100)/100;
+	   	document.getElementById("rotY").innerHTML = "Rot Y :" + Math.round(roty*100)/100;
+	   	document.getElementById("rotZ").innerHTML = "Rot Z :" + Math.round(rotz*100)/100;
+
+	   	
 	   	//console.log("handle accel");
 	};   
